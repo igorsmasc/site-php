7c7618c2f5345189d22181470d38360961617f6e
Igor Santos Mascarenhas <igorsmascarenhas@outlook.com>
2019-05-02 00:51:19 -0300

    Estrutura base + css

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cursos PHP&MYSQL</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<header>
+		<div class="container">
+		<img src="img/logo.png" title="Logo" alt="Logo">
+		<div id="menu"></div>
+		</div>
+	</header>
+
+	<div id="conteudo" class="container">
