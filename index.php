1f45f8572f05bcfd339291d15b09b039544a7e28
Igor Santos Mascarenhas <igorsmascarenhas@outlook.com>
2019-05-02 23:20:12 -0300

    Modularizando as páginas

--- index.php
+++ index.php
@@ -6,8 +6,30 @@ include 'db.php';
 #Cabeçalho
 include 'header.php';
 
-#Conteúdo da´página
-include 'views/home.php';
+#Conteúdo da página
+if(isset($_GET['pagina'])){
+	$pagina = $_GET['pagina'];
+} else {
+	$pagina = 'home';
+}
+
+
+switch ($pagina) {
+	case 'cursos':
+		include 'views/cursos.php';
+		break;
+	case 'alunos':
+		include 'views/alunos.php';
+		break;
+	case 'matriculas':
+		 include 'views/matriculas.php';
+		break;
+	default:
+		include 'views/home.php';
+		break;
+}
+
+
 
 #Rodapé
 include 'footer.php';
